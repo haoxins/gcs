4d0f9a6efb4d793426d1f6320c70c52883b47499
haoxin <haoxinst@gmail.com>
2020-07-22 21:55:23 +0800

    Add big query wrapper

--- bigquery.go
+++ bigquery.go
@@ -1,1 +1,43 @@
 package gcp
+
+import (
+	"context"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/pkg4go/tools"
+	"google.golang.org/api/iterator"
+)
+
+// BigQueryClient The wrapper of BigQuery client
+type BigQueryClient struct {
+	ProjectID string
+}
+
+// GetRowsFromSQL Get the result rows from SQL
+func (c *BigQueryClient) GetRowsFromSQL(sql string) [][]bigquery.Value {
+	ctx := context.Background()
+
+	client, err := bigquery.NewClient(ctx, c.ProjectID)
+	tools.AssertError(err)
+	defer client.Close()
+
+	q := client.Query(sql)
+	iter, err := q.Read(ctx)
+	tools.AssertError(err)
+
+	var rows [][]bigquery.Value
+	for {
+		var row []bigquery.Value
+
+		err := iter.Next(&row)
+		if err == iterator.Done {
+			break
+		}
+
+		tools.AssertError(err)
+
+		rows = append(rows, row)
+	}
+
+	return rows
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module gcp
 
 go 1.14
+
+require (
+	cloud.google.com/go v0.61.0 // indirect
+	cloud.google.com/go/bigquery v1.9.0
+	github.com/pkg4go/tools v1.0.2
+	golang.org/x/sys v0.0.0-20200720211630-cb9d2d5c5666 // indirect
+	golang.org/x/tools v0.0.0-20200721223218-6123e77877b2 // indirect
+	google.golang.org/api v0.29.0
+	google.golang.org/genproto v0.0.0-20200722002428-88e341933a54 // indirect
+)
