b935c978e48ca6a9bcf0da9b4ace9405f7aafdc3
Hao Xin <haoxinst@gmail.com>
2022-04-27 01:01:08 +0800

    Storage - Add WriteString and ReadString

--- storage.go
+++ storage.go
@@ -1,8 +1,10 @@
 package gcp
 
 import (
+	"bytes"
 	"context"
 	"io"
+	"strings"
 	"time"
 
 	"cloud.google.com/go/storage"
@@ -11,35 +13,54 @@ import (
 // StorageClient is a client for Google Cloud Storage
 type StorageClient struct {
 	ProjectID string
-	Timeout   time.Duration
 	Bucket    string
+	Timeout   time.Duration
+	client    *storage.Client
 }
 
-func (c *StorageClient) Write(object string, content io.Reader) error {
-	ctx := context.Background()
-	client, err := storage.NewClient(ctx)
-	if err != nil {
-		return err
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, c.Timeout)
+func (c *StorageClient) Write(object string, source io.Reader) error {
+	ctx, cancel := context.WithTimeout(context.Background(), c.Timeout)
 	defer cancel()
 
-	wc := client.Bucket(c.Bucket).Object(object).NewWriter(ctx)
-	_, err = io.Copy(wc, content)
+	sink := c.client.Bucket(c.Bucket).Object(object).NewWriter(ctx)
+	_, err := io.Copy(sink, source)
 	if err != nil {
 		return err
 	}
 
-	wc.Close()
+	sink.Close()
+
+	return nil
+}
+
+func (c *StorageClient) Read(object string, sink io.Writer) error {
+	ctx, cancel := context.WithTimeout(context.Background(), c.Timeout)
+	defer cancel()
+
+	source, err := c.client.Bucket(c.Bucket).Object(object).NewReader(ctx)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(sink, source)
+	if err != nil {
+		return err
+	}
+
+	source.Close()
 
 	return nil
 }
 
 func (c *StorageClient) WriteString(object string, content string) error {
-	return nil
+	return c.Write(object, strings.NewReader(content))
 }
 
-func (c *StorageClient) ReadString(object string, content string) (string, error) {
-	return "", nil
+func (c *StorageClient) ReadString(object string) (string, error) {
+	buf := new(bytes.Buffer)
+	err := c.Read(object, buf)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
